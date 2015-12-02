180ed043bb5e8e99b245d167b2cb8f93b53e24c1
Adrienne Grieco <adrienne.grieco@gmail.com>
2015-12-01 22:14:49 -0800

    same file as prior with the tests commented out and the actual code working

--- MS3/login_redirect.php
+++ MS3/login_redirect.php
@@ -8,8 +8,8 @@ $_POST['user_id'] = "100";
 $_SESSION['application_id'] = NULL;
 
 // for testing purposes only
-print_r($_POST);
-print_r($_SESSION);
+//print_r($_POST);
+//print_r($_SESSION);
 
 // check if user has created an application in new_application
 $stmt_NewApp = mysqli_prepare($conn, "SELECT application_id FROM new_application WHERE user_id = ?");
@@ -28,27 +28,27 @@ while(mysqli_stmt_fetch($stmt_NewApp)){
 }
 
 // testing
-echo "<p>Application id is ".$application;
+//echo "<p>Application id is ".$application;
 
 mysqli_stmt_close($stmt_NewApp);
 
 // testing
-echo "<p>Count for New_Application is ".$countNewApp;
+//echo "<p>Count for New_Application is ".$countNewApp;
 
 // if the user has created an application, find the state of completion
 if($countNewApp > 0){
 	// get the applcation_id
 	$_SESSION['application_id'] = $application;
 	// check to see if Personal_Information was completed
-	$stmt_PerInf = mysqli_prepare($conn, "SELECT student_fname FROM 
+	$stmt_PerInf = mysqli_prepare($conn, "SELECT user_id FROM 
 	personal_information WHERE application_id = ?");
 	mysqli_stmt_bind_param($stmt_PerInf, "i", $app);
 	$app = $_SESSION['application_id'];
 	mysqli_stmt_execute($stmt_PerInf);
-	mysqli_stmt_bind_result($stmt_PerInf, $name);
+	mysqli_stmt_bind_result($stmt_PerInf, $u_id);
 	
 	//testing
-	echo "<p>Application id is ".$app;
+	//echo "<p>Application id is ".$app;
 	
 	//echo "<p>Student name is ".$name;
 
@@ -61,13 +61,13 @@ if($countNewApp > 0){
 	}
 	
 	// testing
-	echo "<p>Student name is ".$name;
-	echo "<p>Count for Personal_Informaiton is".$countPerInf;
+	//echo "<p>Student name is ".$name;
+	//echo "<p>Count for Personal_Informaiton is".$countPerInf;
 	
 	mysqli_stmt_close($stmt_PerInf);
 
 	// testing
-	echo "<p>Count for Personal_Informaiton is ".$countPerInf;
+	//echo "<p>Count for Personal_Informaiton is ".$countPerInf;
 	
 	// if Personal_Informaiton page completed, check next page
 	if($countPerInf > 0){
@@ -105,7 +105,7 @@ if($countNewApp > 0){
 function goTo_Confirmation(){
 	echo <<<EOF
     <form action="Confirmation.php" method="post">
-<p>
+<p> 
 Our records show that you already have an application on file. 
 </p>
 <p>
@@ -117,36 +117,36 @@ EOF;
 
 function goTo_applicationInfo(){
 	echo "<p>Sent to Application_Information page...";
-	// echo <<<EOF
-    // <form action="Application_Information.php" method="post">
-// <p>
-// Our records show that you already have an application in progress. 
-// </p>
-// <p>
-// Click "Continue" to return to the Application Information page to complete your application.
-// </p>
-// <p><input type=submit value="Continue"></p>
-// EOF;
+	echo <<<EOF
+    <form action="Application_Information.php" method="post">
+<p>
+Our records show that you already have an application in progress. 
+</p>
+<p>
+Click "Continue" to return to the Application Information page to complete your application.
+</p>
+<p><input type=submit value="Continue"></p>
+EOF;
 }
 
 function goTo_personalInfo(){
-	echo "<p>Sent to Personal_Information page...";
-	// echo <<<EOF
-    // <form action="Personal_Informaiton.php" method="post">
-// <p>
-// Our records show that you already have an application in progress. 
-// </p>
-// <p>
-// Click "Continue" to return to the Personal Information page to complete your application.
-// </p>
-// <p><input type=submit value="Continue"></p>
-// EOF;
+	// echo "<p>Sent to Personal_Information page...";
+	echo <<<EOF
+    <form action="Personal_Informaiton.php" method="post">
+<p>
+Our records show that you already have an application in progress. 
+</p>
+<p>
+Click "Continue" to return to the Personal Information page to complete your application.
+</p>
+<p><input type=submit value="Continue"></p>
+EOF;
 }
 
 function goTo_newApplication(){
-	echo "<p>Sent to New_Application page...";
-	// header("Location: New_Application.php");
-    // exit();
+	//echo "<p>Sent to New_Application page...";
+	header("Location: New_Application.php");
+    exit();
 }
 
 
