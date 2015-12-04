cff04f196625e46009088f508380960364d9d90f
brandiweekes <brandiweekes@gmail.com>
2015-12-03 23:13:02 -0800

    Login Page
    New Uploads:
    Login.php --> first page
    CreateNewUserAccount.php --> to create account
    I tested all buttons on login, and they directed to correct pages
    (except view because I do not have a fully entered application--
    it did redirect to the last page of input left off --redirect works!)
    
    Updates:
    New_App, Personal_Info, login_redirect --> removed hard coded tests
    
    Notice: some of the later pages (starting with app_info) still have
     test code in it and was not inserting into my database.
     Specifically,page Personal_Info was not inserted into my database
     at page Application_Info
     I did not investigate because I figure Adrienne knows what is
     going on with that page.

---
+++ MS3/Login.php
@@ -0,0 +1,65 @@
+<?php
+	require "connection.php";
+
+	//prepared statement to insert new_user into DB
+		$stmt_new_user = mysqli_prepare($conn,"INSERT INTO user
+			(user_id,user_password) 
+			VALUES (?,?)");
+		
+		// check connection status
+		if($stmt_new_user==FALSE){die("Connecton failed:".mysqli_connect_error());}
+		
+		mysqli_stmt_bind_param($stmt_new_user, "ss",  
+			$_POST['user_id'],
+			$_POST['user_password']
+			);
+		//execute prepared statement
+		mysqli_stmt_execute($stmt_new_user);
+
+		//close statement and connection
+		mysqli_stmt_close($stmt_new_user);
+		
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+
+<h1>Welcome Applicant!</h1>
+
+<body>
+
+ <form action="Page1_New_Application.php" method="POST">
+
+	<p>
+		Username:
+		<input type="email" name="user_id" placeholder="enter email address">		
+	</p>
+
+	<p>
+		Password:
+		<input type="password" name="user_password" placeholder="enter password">
+	</p>
+
+	<p>
+		<button type="submit" formaction="login_redirect.php" name="create_app">Create Application
+		</button>
+		<button type="submit" formaction="view_redirect.php" name="view_app">View Application
+		</button>
+	</p>
+
+	<p>
+		<h3>New applicant?</h3>
+	</p>
+	<p>
+		Create an account:
+		<button type="submit" formaction="CreateNewUserAccount.php" name="new_user">Create new account
+		</button>
+	</p>
+
+
+</body>
+
+</html>
